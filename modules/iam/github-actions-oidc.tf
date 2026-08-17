cd4701ad5c2c6a8b27ea5a0430ed2b3c8ca4ce8c
Kunal-Jain7 <kunal70223@gmail.com>
2026-08-17 19:49:18 +0530

    Correcting the kubernetes addons and oidc for github

--- modules/iam/github-actions-oidc.tf
+++ modules/iam/github-actions-oidc.tf
@@ -39,13 +39,13 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
 
     condition {
       test     = "StringEquals"
-      variable = "tokens.actions.githubusercontent.com:aud"
+      variable = "token.actions.githubusercontent.com:aud"
       values   = ["sts.amazonaws.com"]
     }
 
     condition {
       test     = "StringEquals"
-      variable = "tokens.actions.githubusercontent.com:sub"
+      variable = "token.actions.githubusercontent.com:sub"
       values = [
         "repo:${var.github_org}/frontend:ref:refs/heads/main",
         "repo:${var.github_org}/frontend:ref:refs/heads/develop",
