e90a04a8bc0c1cf7b44942a6224fa92f9d0c889a
Kunal-Jain7 <kunal70223@gmail.com>
2026-08-17 17:45:15 +0530

    Updated the module in the EKS main file

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -25,7 +25,7 @@ module "eks" {
   project            = local.project
   env                = local.env
   vpc_id             = module.vpc.vpc_id
-  subnet_ids         = module.vpc.private_subnet
+  subnet_ids         = module.vpc.private_subnets
   kubernetes_version = "1.33"
   instance_types     = ["t3.small"]
   min_size           = 1
